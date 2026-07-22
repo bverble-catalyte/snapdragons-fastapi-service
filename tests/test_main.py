06c8aa9b67988413b8fc044693103a48cff8b63d
Brennan Idor <bidor@catalyte.io>
2026-07-22 15:52:26 -0500

    Add test case for /products path operation function handling POST requests

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,7 +10,14 @@ def test_read_root():
     assert response.json() == {"message": "Hello World"}
 
 def test_hello_name():
-    name = "Jimmie"
     response = client.get("/hello/Jimmie")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, Jimmie!"}
+
+def test_create_product():
+    product_test = {"name": "Basil Plant - 4in Pot", "unit": "each", "cost_per_unit": 1.75, "price_per_unit": 4.99, "quantity_in_stock": 40}
+    response = client.post("/products", json = product_test)
+    assert response.status_code == 201
+    data = response.json()
+    assert "name" and "unit" and "cost_per_unit" and "price_per_unit" and "quantity_in_stock" in data
+    assert isinstance(data["name"], str)
