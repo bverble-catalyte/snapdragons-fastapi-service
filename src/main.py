827c6a48547b738e938a7826817476c90e6f5139
Brennan Idor <bidor@catalyte.io>
2026-07-23 13:00:05 -0500

    Added function documentation for search_products

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from typing import Annotated, List
-from fastapi import FastAPI, status, Query
+
+from fastapi import FastAPI, Query, status
+
 from database import temp_storage
 from models.product import Product
 
@@ -23,14 +25,26 @@ def view_products():
 
 @app.get("/products/search")
 def search_products(
-    name: Annotated[str, Query(description = "The product name is required.")],
-    unit: Annotated[str | None, Query(description = "Optional product unit.")] = None
+    name: Annotated[str, Query(description="The product name is required.")],
+    unit: Annotated[str | None, Query(description="Optional product unit.")] = None,
 ):
+    """Handles GET requests for /products/search by searching for matching products in-memory.
+
+    Args:
+        name (str): The product name.
+        unit (str): The product's unit of sale; optional.
+
+    Returns:
+        A list of products with a matching name and unit.
+    """
+
+    # Helper: Normalizes the formatting of strings for search.
     def normalize(s: str) -> str:
         return "".join(s.lower().split())
 
     return [
-        product for product in temp_storage
+        product
+        for product in temp_storage
         if normalize(name) in normalize(product["name"])
         and (unit is None or product["unit"] == unit)
     ]
@@ -41,7 +55,7 @@ def create_product(product: Product):
     """Handles POST requests for /products endpoint and appends product for in-memory storage.
 
     Args:
-        product: A valid product.
+        product (Product): A valid product.
 
     Returns:
         Status Code - 201 Created with sent data.
