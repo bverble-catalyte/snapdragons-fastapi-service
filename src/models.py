9434325b0ab7a425ed5e86d8f7ede9c7d146b2a4
Bryan Verble <bverble@catalyte.io>
2026-07-27 14:06:11 -0500

    Add ProductRead model

--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from typing import Annotated
 
-from pydantic import BaseModel, Field, StringConstraints
+from pydantic import BaseModel, ConfigDict, Field, PositiveInt, StringConstraints
 from sqlalchemy import Identity, Integer, Numeric
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -24,6 +24,28 @@ class ProductCreate(BaseModel):
     cost_per_unit: Annotated[Decimal, Field(gt=0, decimal_places=2)]
     price_per_unit: Annotated[Decimal, Field(gt=0, decimal_places=2)]
     quantity_in_stock: Annotated[Decimal, Field(ge=0)]
+
+
+class ProductRead(BaseModel):
+    """Represents a product sold by the garden center.
+
+    Attributes:
+        id: The unique product identifier
+        name: The product name
+        unit: The product's unit of sale (e.g. "each", "bag", "lb")
+        cost_per_unit: Amount the garden center pays suppliers, in dollars per unit
+        price_per_unit: Amount the garden center charges customers, in dollars per unit
+        quantity_in_stock: Current amount of product in inventory, in stock units
+    """
+
+    id: PositiveInt
+    name: Annotated[str, StringConstraints(min_length=1, strip_whitespace=True)]
+    unit: Annotated[str, StringConstraints(min_length=1, strip_whitespace=True)]
+    cost_per_unit: Annotated[Decimal, Field(gt=0, decimal_places=2)]
+    price_per_unit: Annotated[Decimal, Field(gt=0, decimal_places=2)]
+    quantity_in_stock: Annotated[Decimal, Field(ge=0)]
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class Product(Base):
