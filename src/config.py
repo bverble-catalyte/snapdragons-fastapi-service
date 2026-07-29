c72a5d85138028962d643dbaa2b1fabcbb0732b2
Brennan Idor <bidor@catalyte.io>
2026-07-29 11:18:23 -0500

    Added explicit path to .env

--- src/config.py
+++ src/config.py
@@ -1,8 +1,10 @@
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
+env_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(dotenv_path=env_path)
 
 
 def get_required_env(name: str) -> str:
