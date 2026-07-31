90e5a6b2aa15e4f8dfcabee707106cfde2a1bc54
Bryan Verble <bverble@catalyte.io>
2026-07-31 11:40:40 -0500

    Order tests according to CRUD acronym

--- tests/test_product_routes.py
+++ tests/test_product_routes.py
@@ -94,47 +94,6 @@ def test_view_product_on_nonexistant_product_should_return_404(
     assert response.status_code == 404
 
 
-def test_delete_product_should_remove_product_from_all_endpoint_responses(
-    authenticated_client, db_session, seed_product, first_existing_product
-):
-    eid = first_existing_product.id
-
-    view_one_before = authenticated_client.get(f"/products/{eid}")
-    search_query = view_one_before.json()["name"][:3]
-    view_all_before = authenticated_client.get("/products")
-    search_before = authenticated_client.get("/products", params={"name": search_query})
-
-    response = authenticated_client.delete(f"/products/{eid}")
-    assert response.status_code == 204
-
-    view_one_after = authenticated_client.get(f"/products/{eid}")
-    view_all_after = authenticated_client.get("/products")
-    search_after = authenticated_client.get("/products", params={"name": search_query})
-
-    assert view_one_after.status_code == 404
-    assert len(view_all_before.json()) - 1 == len(view_all_after.json())
-    assert len(search_before.json()) - 1 == len(search_after.json())
-
-
-def test_delete_product_on_nonexistant_product_should_return_404(
-    authenticated_client, db_session, seed_product, unused_product_id
-):
-    response = authenticated_client.delete(f"/products/{unused_product_id}")
-    assert response.status_code == 404
-
-
-def test_delete_product_should_keep_product_in_database(
-    authenticated_client, db_session, seed_product, first_existing_product
-):
-    get_response = authenticated_client.get(f"/products/{first_existing_product.id}")
-    delete_response = authenticated_client.delete(
-        f"/products/{first_existing_product.id}"
-    )
-    assert delete_response.status_code == 204
-    product = db_session.get(Product, first_existing_product.id)
-    assert product.name == get_response.json()["name"]
-
-
 def test_update_product_should_update_product(
     authenticated_client, db_session, seed_product, first_existing_product
 ):
@@ -210,3 +169,44 @@ def test_update_product_should_not_update_deleted_products(
     assert response.status_code == 404
     product = db_session.get(Product, first_existing_product.id)
     assert product == first_existing_product
+
+
+def test_delete_product_should_remove_product_from_all_endpoint_responses(
+    authenticated_client, db_session, seed_product, first_existing_product
+):
+    eid = first_existing_product.id
+
+    view_one_before = authenticated_client.get(f"/products/{eid}")
+    search_query = view_one_before.json()["name"][:3]
+    view_all_before = authenticated_client.get("/products")
+    search_before = authenticated_client.get("/products", params={"name": search_query})
+
+    response = authenticated_client.delete(f"/products/{eid}")
+    assert response.status_code == 204
+
+    view_one_after = authenticated_client.get(f"/products/{eid}")
+    view_all_after = authenticated_client.get("/products")
+    search_after = authenticated_client.get("/products", params={"name": search_query})
+
+    assert view_one_after.status_code == 404
+    assert len(view_all_before.json()) - 1 == len(view_all_after.json())
+    assert len(search_before.json()) - 1 == len(search_after.json())
+
+
+def test_delete_product_on_nonexistant_product_should_return_404(
+    authenticated_client, db_session, seed_product, unused_product_id
+):
+    response = authenticated_client.delete(f"/products/{unused_product_id}")
+    assert response.status_code == 404
+
+
+def test_delete_product_should_keep_product_in_database(
+    authenticated_client, db_session, seed_product, first_existing_product
+):
+    get_response = authenticated_client.get(f"/products/{first_existing_product.id}")
+    delete_response = authenticated_client.delete(
+        f"/products/{first_existing_product.id}"
+    )
+    assert delete_response.status_code == 204
+    product = db_session.get(Product, first_existing_product.id)
+    assert product.name == get_response.json()["name"]
